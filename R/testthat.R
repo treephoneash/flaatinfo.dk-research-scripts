4f0a1f8a6239e3243907e8e1a3ab55f76e116a39
treephoneash <tryfondimitriou@gmail.com>
2026-08-21 18:43:38 +0300

    Add files via upload

---
+++ R/testthat.R
@@ -0,0 +1,8 @@
+library(testthat)
+
+source(file.path("R", "epidemiology_shared_helpers.R"))
+source(file.path("R", "human_demographic_models.R"))
+source(file.path("R", "epidemiology_result_figures.R"))
+source(file.path("R", "landscape_ecological_analyses.R"))
+
+test_dir("tests/testthat")
